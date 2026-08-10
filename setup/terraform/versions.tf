3d370dce0d023719eefc672de00dee647f941615
LingamSravan63 <sravanlingam6302@gmail.com>
2026-08-10 12:32:12 +0000

    Add GitHub Actions workflows and configure project

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -1,9 +1,10 @@
 provider "aws" {
+
   region = "us-east-1"
 }
 
 terraform {
-  required_version = "1.3.9"
+  required_version = "1.4.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
